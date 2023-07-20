e21e461119d6bb921bb1c75b95946c2ecc16d660
hikmawanaz <hikmawan@mail.com>
2023-07-20 14:43:18 +0700

    feat: initial

---
+++ config/config.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+const { DB_NAME, DB_USERNAME, DB_PASSWORD, DB_HOSTNAME } = process.env;
+module.exports = {
+  development: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOSTNAME,
+    dialect: "mysql",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
